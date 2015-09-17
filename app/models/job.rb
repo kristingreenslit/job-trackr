fad18847b1941ac22dcf68061116475707083202
KristinGreenslit <kristingreenslit@gmail.com>
2015-09-17 17:43:11 -0400

    devise styles MVP

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,14 @@
 class Job < ActiveRecord::Base
+
+	def aging
+	  self.jobs.each do |job|
+	      if job.app_dt > 30
+	        puts "30"
+	      elsif job.app_dt > 14
+	        puts "14"
+	      elsif job.app_dt > 7
+	        puts "7"
+	   	  end  
+	   end
+	end
 end
