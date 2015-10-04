0850e64aba1ce855c946bc20e6cefeefb0848af7
KristinGreenslit <kristingreenslit@gmail.com>
2015-10-04 19:24:06 -0400

    remove rails form helper w/datepicker

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,4 @@
 class Job < ActiveRecord::Base
 	belongs_to :user
-	validates :app_dt, format: { with: /\A\d{4}-\d{2}-\d{2}\z/,
-    message: " - format is not correct" }
-	
+	validates :app_dt, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: " - format is not correct" }
 end
